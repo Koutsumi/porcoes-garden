a10c59bcb33bc1322385d274a4aa5214fac7990f
Fernanda <febaccarini@gmail.com>
2022-06-11 21:31:27 -0300

    favicon copy function

--- script/whats.js
+++ script/whats.js
@@ -2,7 +2,7 @@ function whatsapp(){
     var pedido = document.getElementById("textArea").value;
     //var valor = document.getElementById("valorPagar").innerText;
     var a = document.getElementById("a");
-    a.setAttribute("href", "https://api.whatsapp.com/send?phone=5511997895078&text=" + pedido +"%0D"+"%0D");
+    a.setAttribute("href", "https://api.whatsapp.com/send?phone=5511982608171&text=" + pedido +"%0D"+"%0D");
   
     console.log('teste')
   }
